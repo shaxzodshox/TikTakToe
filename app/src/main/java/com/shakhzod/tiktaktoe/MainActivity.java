b28ba704101fadc03206706933dd5b75b710b124
shaxzodshox <shaxzod-original@mail.ru>
2020-01-29 14:55:21 +0500

    Game functions are ready

--- app/src/main/java/com/shakhzod/tiktaktoe/MainActivity.java
+++ app/src/main/java/com/shakhzod/tiktaktoe/MainActivity.java
@@ -1,19 +1,28 @@
 package com.shakhzod.tiktaktoe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     // 0 = yellow, 1 = red
     int activePlayer = 0;
 
+    boolean gameIsActive = true;
+
     //2 means unplayed and 9 elements mean 9 items
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPositions = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8},
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8},
+            {0, 4, 8}, {2, 4, 6}};
 
 
     @Override
@@ -29,21 +38,73 @@ public class MainActivity extends AppCompatActivity {
 
         int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        if(gameState[tappedCounter] == 2){
+        if (gameState[tappedCounter] == 2 && gameIsActive) {
 
             gameState[tappedCounter] = activePlayer;
 
-        counter.setRotation(0);
-        counter.setTranslationY(-1000f);
+            counter.setRotation(0);
+            counter.setTranslationY(-1000f);
 
-        if (activePlayer == 0) {
-            counter.setImageResource(R.drawable.yellow);
-            activePlayer = 1;
-        } else {
-            counter.setImageResource(R.drawable.red);
-            activePlayer = 0;
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(500);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]]
+                        && gameState[winningPosition[1]] == gameState[winningPosition[2]]
+                        && gameState[winningPosition[0]] != 2) {
+
+                    //Default
+                    String winner = "Red";
+
+                    if (gameState[winningPosition[0]] == 0) {
+                        winner = "Yellow";
+                    }
+
+                    //Someone has won!
+                    gameIsActive = false;
+
+                    TextView winnerMessage = findViewById(R.id.winnerMessage);
+                    winnerMessage.setText(winner + " has won!");
+
+                    LinearLayout linearLayout = findViewById(R.id.playAgainLayout);
+                    linearLayout.setVisibility(View.VISIBLE);
+                }
+                else{
+                    boolean gameIsOver = true;
+                    for(int counterState : gameState){
+                        if(counterState == 2) gameIsOver = false;
+                    }
+                    if(gameIsOver){
+                        TextView winnerMessage = findViewById(R.id.winnerMessage);
+                        winnerMessage.setText("It is a draw!");
+
+                        LinearLayout linearLayout = findViewById(R.id.playAgainLayout);
+                        linearLayout.setVisibility(View.VISIBLE);
+                    }
+                }
+            }
         }
-        counter.animate().translationYBy(1000f).rotation(360).setDuration(500);
+    }
+
+    public void playAgain(View view) {
+        gameIsActive = true;
+        LinearLayout linearLayout = findViewById(R.id.playAgainLayout);
+        linearLayout.setVisibility(View.INVISIBLE);
+
+        activePlayer = 0;
+        for (int i = 0; i < gameState.length; i++) {
+            gameState[i] = 2;
+        }
+
+        GridLayout layout = findViewById(R.id.gridLayout);
+        for (int i = 0; i < layout.getChildCount(); i++){
+            ((ImageView) layout.getChildAt(i)).setImageResource(0);
         }
     }
 }
